d1c0ab3cade282a33dcf2ea4021b7aa1154b509e
Seth Johnson <SethJohnsonCoding@gmail.com>
2023-04-28 14:59:12 -0700

    comments for recalection and clarity

--- app.js
+++ app.js
@@ -10,10 +10,14 @@ let total = 0;
 let inputValue = '';
 
 /* Functions */
+
+// updates the innerText property of the displayNumber element with the current value of total.
 function updateTotal() {
     displayNumber.innerText = total;
 }
 
+// that takes a value and checks if it can be converted to a number using parseInt()
+// If it can be converted, it returns true, otherwise, it returns false.
 function isNumeric(value) {
     // Try to convert the value to a number using parseInt()
     const num = parseInt(value);
@@ -26,6 +30,8 @@ function isNumeric(value) {
     }
 }
 
+// adds the current inputValue to the total variable, also updating the displayNumber and displayInput elements
+// If inputValue is not a number, display the NaN string in the displayNumber element.
 function addValue() {
     if (isNumeric(inputValue)) {
         total += parseInt(inputValue);
@@ -36,6 +42,8 @@ function addValue() {
     }
 }
 
+// subtracts the current inputValue from the total variable also updating the displayNuber and inputValue elements
+// If inputValue is not a number display the NAN string in the displayNumber element
 function subValue() {
     if (isNumeric(inputValue)) {
         total -= parseInt(inputValue);
@@ -46,9 +54,14 @@ function subValue() {
     }
 }
 
+// listens for input events, and whenever the input value changes and updates the inputValue variable 
+// 
 displayInput.addEventListener('input', function(event) {
     inputValue = event.target.value;
 });
 
+/*Event Listeners*/
+
+// both listen for click events calling addValue and subValue functions if their buttons are clicked
 addBTN.addEventListener('click', addValue);
 subBTN.addEventListener('click', subValue);
